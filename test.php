bc90b9a63d45939ff66a767cf110b9ea54bc54be
abhaya <abhaya.punkid@gmail.com>
2024-02-08 10:23:58 +0545

    fixed yours!!!!!

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
 <?php
 
-echo("hello da");
-echo("how is going")
-echo("how did it feel being wrong? Good?");
-echo("hello da");
 
+echo("hello da");
+echo("how is going");
+echo("how did it feel being wrong? Good?");
+echo("hello da how is going how did it feel being wrong? Good?");
+
+echo("hello da");
 ?>
